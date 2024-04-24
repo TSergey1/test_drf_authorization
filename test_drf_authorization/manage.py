55b05c3f18cbf63e69e796f58ee0b15c167e599a
Сергей Тыртычный <tyrtychnyy90@mail.ru>
2024-04-24 21:42:21 +0300

    1

--- test_drf_authorization/manage.py
+++ test_drf_authorization/manage.py
@@ -6,7 +6,8 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'test_drf_authorization.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                          'test_drf_authorization.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
