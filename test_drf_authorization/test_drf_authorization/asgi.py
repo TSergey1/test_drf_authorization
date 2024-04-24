1672d27e93522ca79a255d40f0aec56a71c927ea
Сергей Тыртычный <tyrtychnyy90@mail.ru>
2024-04-24 21:42:38 +0300

    2

--- test_drf_authorization/test_drf_authorization/asgi.py
+++ test_drf_authorization/test_drf_authorization/asgi.py
@@ -2,6 +2,7 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'test_drf_authorization.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                      'test_drf_authorization.settings')
 
 application = get_asgi_application()
